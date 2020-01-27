13071e7fcb53058db9f40d5b650cd52196501d81
t-kusakabe <ttmac0704@gmail.com>
2020-01-27 15:24:33 +0900

    api-gatewayとlambdaのstack

--- lib/sample-cdk-sample-app-stack.ts
+++ lib/sample-cdk-sample-app-stack.ts
@@ -1,7 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class SampleCdkSampleAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Lambda
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.asset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    // API Gateway
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
